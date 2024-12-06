d79a6b988f38a5f550ccc56f3ce6880d2bce0770
Daniil Fadeev <dafadeev_1@edu.hse.ru>
2024-12-06 21:43:14 +0300

    создал тестовую бд

--- fruitsDataBase/init/init.sql
+++ fruitsDataBase/init/init.sql
@@ -1,11 +1,59 @@
+-- Создание таблицы пользователей
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    profile_picture TEXT
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    profile_picture VARCHAR(255)
 );
 
-INSERT INTO users (username, email, password, profile_picture)
-VALUES ('tree_lover', 'tree_lover@example.com', 'hashed_password', '/images/profile/tree_lover.jpg');
+-- Таблица видов деревьев
+CREATE TABLE tree_species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_url VARCHAR(255) -- Ссылка на изображение или фото
+);
+
+-- Обновление таблицы деревьев
+CREATE TABLE plants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    species_id INT REFERENCES tree_species(id) ON DELETE SET NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    tree_logo VARCHAR(255)
+);
+
+-- Создание таблицы всех достижений
+CREATE TABLE achievements (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+-- Создание таблицы достижений
+CREATE TABLE user_achievements (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    achievement_id INT REFERENCES achievements(id) ON DELETE CASCADE,
+    unlocked_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Создание таблицы мест с координатами
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Создание таблицы журналов активности
+CREATE TABLE activity_logs (
+    id SERIAL PRIMARY KEY,
+    action VARCHAR(255) NOT NULL,
+    action_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE
+);
