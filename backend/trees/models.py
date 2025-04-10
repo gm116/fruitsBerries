6d061bdfffb489c9666bb122551a66c32dd2dd22
Daniil Fadeev <dafadeev_1@edu.hse.ru>
2025-04-10 18:00:28 +0300

    refactor: убрал лишние файлы + поправил код визуально

--- backend/trees/models.py
+++ backend/trees/models.py
@@ -8,7 +8,7 @@ class Species(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     image_url = models.CharField(max_length=255, blank=True, null=True)
-    category = models.CharField(max_length=50, choices=[("tree", "Tree"), ("bush", "Иush")])
+    category = models.CharField(max_length=50, choices=[("tree", "Tree"), ("bush", "Bush")])
 
     def __str__(self):
         return self.name
